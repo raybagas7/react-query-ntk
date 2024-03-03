9e08c6e43848d598ae58dca340a0840dbf0cbb45
raybagas7 <raysam.gas@gmail.com>
2024-03-03 12:44:09 +0700

    feat-exp(fetching): - give a promise response type in react query - give condition in loading, error and success state fetching - show the data result to a list

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,36 @@
+'use client';
 import SelectedSegment from '@/components/SelectedSegment';
+import Link from 'next/link';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
   return (
-    <section>
-      <nav className="p-3 bg-blue-600">NAVIGATION</nav>
-      <SelectedSegment />
-      <div>{children}</div>
-    </section>
+    <QueryClientProvider client={queryClient}>
+      <section>
+        <nav className="p-3 bg-blue-600 flex justify-between">
+          <div>
+            <p>NAVIGATION</p>
+          </div>
+          <div>
+            <ul className="flex gap-3">
+              <li>
+                <Link href={'/'}>Home</Link>
+              </li>
+              <li>
+                <Link href={'/dashboard'}>Dahsboard</Link>
+              </li>
+              <li>
+                <Link href={'/dashboard/setting'}>Setting</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <SelectedSegment />
+        <div>{children}</div>
+      </section>
+    </QueryClientProvider>
   );
 };
 
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,51 @@
+'use client';
 import React from 'react';
+import { useQuery } from 'react-query';
+
+interface Todo {
+  id: number;
+  todo: string;
+  completed: boolean;
+  userId: number;
+}
+interface TodosRes {
+  todos: Todo[];
+}
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { isLoading, error, data } = useQuery(
+    'todos',
+    (): Promise<TodosRes> =>
+      fetch('https://dummyjson.com/todos').then((res) => res.json())
+  );
+
+  console.log(data);
+
+  if (isLoading) {
+    return (
+      <div>
+        <p className="animate-pulse">Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>Error</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {data === undefined ? (
+        <p>No todos</p>
+      ) : (
+        data.todos.map((data) => <li key={data.id}>{data.todo}</li>)
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
